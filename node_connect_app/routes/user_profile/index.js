0e19e5247eb94666b8f8148189fe8fb809cfae48
mary <maradon853@gmail.com>
2020-09-26 13:03:46 -0500

    adding code for uploading pic

--- node_connect_app/routes/user_profile/index.js
+++ node_connect_app/routes/user_profile/index.js
@@ -1,7 +1,9 @@
 const User = require("../../database_models/user_model");
+const shortid = require("shortid");
+const fs = require("fs");
+const UserStatus = require("../../database_models/user_status_model");
 
-
-module.exports.register = function(plugin,options,next){
+exports.register = function(plugin,options,next){
     plugin.route([
         {
             method: "GET",
@@ -32,7 +34,34 @@ module.exports.register = function(plugin,options,next){
                             }else{
                                 reply();
                             }
-                        })
+                        });
+                    });
+                }
+            }
+        },
+        {
+            method: "POST",
+            path: "/profile_pic/upload",
+            config: {
+                auth: "simple-cookie-strategy",
+                handler: function(request,reply){
+                    console.log("image_data", request.payload.image_data);
+                    var user_profile_image = "user_" + request.auth.credentials.member_id + "_" + shortid.generate() + "." + request.payload.image_type;
+                    fs.writeFile("user_profile_images/" + user_profile_image, new Buffer(request.payload.image_data, "base64"), function(err){
+                        if (!err){
+                            User.findOne({"email": request.auth.credentials.user}, function(err, user){
+                                user.user_profile[0].profile_pic = user_profile_image;
+                                user.save(function(err){
+                                    if(err){
+                                        reply(err)
+                                    }else{
+                                        UserStatus.update({"user_email": request.auth.credentials.user}, {"profile_pic": user_profile_image}, function(err,result){
+                                            reply(user_profile_image);
+                                        });
+                                    }
+                                })
+                            });
+                        }
                     });
                 }
             }
