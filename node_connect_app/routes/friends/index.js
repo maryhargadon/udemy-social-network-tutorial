2727343ef640dc7bd48a00c20917d70a4dc8372f
mary <maradon853@gmail.com>
2020-10-01 18:11:14 -0500

    adding visit friend profile features

--- node_connect_app/routes/friends/index.js
+++ node_connect_app/routes/friends/index.js
@@ -16,18 +16,37 @@ exports.register = function(plugin, options, next){
             }
         },
         {
-            method: "GET",
+            method: "POST",
             path: "/friend_request",
             config: {
                 auth: "simple-cookie-strategy",
                 handler: function(request, reply){
                     User.find({"email": request.auth.credentials.user}, function(err, sending_user){
                         User.find({"member_id": request.payload.friend_member_id}, function(err, potential_friend){
-                            potential_friend[0].update({$push: {"friend_requests": {"member_id": sending_user[0].member_id, "friend_name": sending_user[0].name, "profile_pic": sending_user[0].user_profile[0].profile_pic}}}, function(err){
+                            potential_friend[0].update({$push: {"friend_requests": {"member_id": sending_user[0].member_id, "friend_name": sending_user[0].name , "profile_pic": sending_user[0].user_profile[0].profile_pic}}}, function(err){
                                 reply();
                             });   
                         });
                     });
+                }
+            }
+        },
+        {
+            method: "POST",
+            path: "/accept_friend_request",
+            config: {
+                auth: "simple-cookie-strategy",
+                handler: function(request, reply){
+                   User.find({"email": request.auth.credentials.user}, function(err, user){
+                    User.find({"member_id": request.payload.member_id}, function(err, accepted_friend_user){
+                       user[0].update({$push: {"friends": {"member_id": accepted_friend_user[0].member_id, "friend_name": accepted_friend_user[0].name, "profile_pic": accepted_friend_user[0].user_profile[0].profile_pic}}, $pull: {"friend_requests": {member_id: request.payload.member_id}}},
+                       function(err){
+                        accepted_friend_user[0].update({$push: {"friends": {"member_id": user[0].member_id, "friend_name": user[0].name, "profile_pic": user[0].user_profile[0].profile_pic}}},
+                         function(err){
+                       });
+                    });
+                    });
+                   });
                 }
             }
         }
--- node_connect_app/routes/user_profile/index.js
+++ node_connect_app/routes/user_profile/index.js
@@ -11,12 +11,15 @@ exports.register = function(plugin, options, next){
             config: {
                 auth: "simple-cookie-strategy",
                 handler: function(request, reply){
-                    User.findOne({"email": request.auth.credentials.user}, function(err,user){
-                        reply.view("user_profile",{user: user});
+                    User.findOne({"email": {$ne: request.auth.credentials.user}}, function(err,user){
+                        reply.view("user_profile", {user_profile: user});
                     });
+                    // User.findOne({"email": request.auth.credentials.user}, function(err,user){
+                    //     reply.view("user_profile",{user: user});
+                    //});
                 }
             }
-        },
+         },
         {
             method: "POST",
             path: "/user_profile/edit",
@@ -45,23 +48,48 @@ exports.register = function(plugin, options, next){
             config: {
                 auth: "simple-cookie-strategy",
                 handler: function(request,reply){
-                    console.log("image_data", request.payload.image_data);
                     var user_profile_image = "user_" + request.auth.credentials.member_id + "_" + shortid.generate() + "." + request.payload.image_type;
-                    fs.writeFile("user_profile_images/" + user_profile_image, new Buffer(request.payload.image_data, "base64"), function(err){
-                        if (!err){
+                    fs.writeFile("user_profile_images/" + user_profile_image, Buffer(request.payload.image_data, "base64"), function(err){
+                        if (err){
+                            reply().code(400);
+                        }else{
                             User.findOne({"email": request.auth.credentials.user}, function(err, user){
                                 user.user_profile[0].profile_pic = user_profile_image;
                                 user.save(function(err){
                                     if(err){
-                                        reply(err)
+                                        reply().code(400);
                                     }else{
-                                        UserStatus.update({"user_email": request.auth.credentials.user}, {"profile_pic": user_profile_image}, function(err,result){
-                                            reply(user_profile_image);
+                                        UserStatus.update({"user_email": request.auth.credentials.user}, {"profile_pic": user_profile_image}, {multi: true}, function(){
+                                            if(err){
+                                            reply().code(400);
+                                    }else{
+                                        reply();
+                                    }
                                         });
                                     }
                                 })
                             });
                         }
+                    });
+                }
+            }
+        },
+        {
+            method: "GET",
+            path: "/user_profile/{member_id}",
+            config: {
+                auth: "simple-cookie-strategy",
+                handler: function(request,reply){
+                    User.find({"email": request.auth.credentials.user}, function(err,user){
+                        var all_user_friends = user[0].friends;
+                        var request_profile_member_id = request.params.member_id;
+                        all_user_friends.forEach(function(friend){
+                            if(friend.member_id === request_profile_member_id){
+                                User.findOne({"member_id": friend.member_id}, function(err,visting_friend){
+                                    reply.view("user_profile_visit", {user: visiting_friend});
+                                });
+                            }
+                        });
                     });
                 }
             }
